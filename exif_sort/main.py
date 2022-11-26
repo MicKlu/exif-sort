8a3eaaa74e1d56dbe6dc50f58d7b72d0c92fce07
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2022-11-26 19:11:40 +0100

    Perform dry sorting (this time from GUI)

--- exif_sort/main.py
+++ exif_sort/main.py
@@ -6,6 +6,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLineEdit
 from exif_sort.ui.main_window import Ui_MainWindow
 
+from exif_sort.sorter import ImageSorter
+
 class MainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent=None):
@@ -30,25 +32,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.renameFormatComboBox.currentTextChanged.connect(self.__on_rename_format_change)
         self.renameCheckBox.clicked.connect(self.__on_rename_click)
 
+        # Start/cancel sorting
+        self.sortButton.clicked.connect(self.__on_sort_button_click)
+        self.cancelButton.clicked.connect(self.__on_cancel_button_click)
+
         # Quit application
         self.actionQuit.triggered.connect(self.__quit)
 
     def __log_status(self, msg: str):
+        """Displays message in status listbox"""
         self.statusList.addItem(msg)
+        self.statusList.scrollToBottom()
 
     def __on_input_directory_button_click(self):
-        """Called when input's "Browse" button has been clicked"""
-
+        """Called when input's "Browse" button has been clicked."""
         self.__browse_in_out_directory(self.inputDirectoryPathEdit)
 
     def __on_output_directory_button_click(self):
-        """Called when output's "Browse" button has been clicked"""
-
+        """Called when output's "Browse" button has been clicked."""
         self.__browse_in_out_directory(self.outputDirectoryPathEdit)
 
     def __browse_in_out_directory(self, path_edit: QLineEdit):
-        """Updates path field"""
-
+        """Updates path field."""
         path = QFileDialog.getExistingDirectory(self, directory=path_edit.text())
 
         if len(path) == 0:
@@ -57,28 +62,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         path_edit.setText(path)
 
     def __on_output_directory_path_change(self, text: str):
-        """Called when output path has been changed"""
-
+        """Called when output path has been changed."""
         self.__update_output_preview()
 
     def __on_output_format_change(self, text: str):
-        """Called when output format has been changed"""
-
+        """Called when output format has been changed."""
         self.__update_output_preview()
 
     def __on_rename_format_change(self, text: str):
-        """Called when rename format has been changed"""
-
+        """Called when rename format has been changed."""
         self.__update_output_preview()
 
     def __on_rename_click(self, checked: bool):
-        """Called when renaming files has been toggled"""
-
+        """Called when renaming files has been toggled."""
         self.__update_output_preview()
 
     def __update_output_preview(self):
-        """Updates output preview"""
-
+        """Updates output preview."""
         date = datetime.now()
 
         # Get output path
@@ -89,19 +89,87 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             output_path = Path.home()
 
         # Add options format
-        sort_format = self.outputFormatComboBox.currentText()
+        sort_format = self.outputFormatComboBox.currentText().strip()
         output_path = output_path.joinpath(date.strftime(sort_format))
 
         # Add rename format
         if self.renameCheckBox.isChecked():
-            rename_format = self.renameFormatComboBox.currentText()
+            rename_format = self.renameFormatComboBox.currentText().strip()
             output_path = output_path.joinpath(date.strftime(rename_format))
         else:
             output_path = output_path.joinpath("image")
 
         self.outputPreviewValueLabel.setText(str(output_path) + ".jpg")
 
+    def __on_sort_button_click(self):
+        """Called when "Sort" button has been clicked."""
+        # Get input path
+        input_path = self.inputDirectoryPathEdit.text().strip()
+        if len(input_path) == 0:
+            self.__log_status("Invalid input directory")
+            return
+
+        input_path = Path(input_path)
+
+        # Get output path
+        output_path = self.outputDirectoryPathEdit.text().strip()
+
+        if len(output_path) > 0:
+            output_path = Path(output_path)
+        else:
+            output_path = Path.home()
+
+        # Initialize sorter
+        sorter = ImageSorter(input_path)
+        sorter.output_dir = output_path
+        sorter.recursive = self.recursiveCheckBox.isChecked()
+        sorter.group_format = self.outputFormatComboBox.currentText().strip()
+        sorter.sort_unknown = not self.skipUnknownCheckBox.isChecked()
+
+        if self.renameCheckBox.isChecked():
+            sorter.rename_format = self.renameFormatComboBox.currentText().strip()
+
+        sorter.on_finish = self.__on_sort_finish
+        sorter.on_move = self.__on_sort_move
+        sorter.on_skip = self.__on_sort_skip
+        sorter.on_error = self.__on_sort_error
+
+        # Toggle Cancel button
+        self.sortButton.setVisible(False)
+        self.cancelButton.setVisible(True)
+
+        # Start sorting
+        sorter.sort()
+
+    def __on_sort_finish(self):
+        """Called once sorting finishes."""
+        # Toggle Sort button
+        self.sortButton.setVisible(True)
+        self.cancelButton.setVisible(False)
+
+    def __on_sort_move(self, old_path: str, new_path: str):
+        """Called when file has been moved."""
+        self.__log_status(f"Moved \"{old_path}\" to \"{new_path}\"")
+
+    def __on_sort_skip(self, path: str):
+        """Called when file has been skipped."""
+        self.__log_status(f"Skipped \"{path}\"")
+
+    def __on_sort_error(self, exception: Exception):
+        msg = "Error: "
+        if isinstance(exception, PermissionError):
+            msg += f"Permission denied to {exception.filename}"
+        elif isinstance(exception, FileNotFoundError):
+            msg += f"{exception.filename} not found"
+
+        self.__log_status(msg)
+
+    def __on_cancel_button_click(self):
+        """Called when "Cancel" button has been clicked."""
+        pass
+
     def __quit(self):
+        """Closes window and exits application."""
         self.close()
 
 def main():
--- exif_sort/sorter.py
+++ exif_sort/sorter.py
@@ -1,24 +1,24 @@
 import copy
 from datetime import datetime
 from pathlib import Path
-from PIL import Image
 import shutil
+from typing import Callable
+
+from PIL import Image
 
 class ImageOpenError(Exception):
-    """Raised if image couldn't be opened"""
-
+    """Raised if image couldn't be opened."""
     def __init__(self, path):
         super().__init__(f"Couldn't open image ({path})")
 
 class ImageFile:
-    """Class for operating on image file"""
-
+    """Class for operating on image file."""
     def __init__(self, path: Path):
         self.__path = path
+        self.__exif = None
 
     def get_date_time(self) -> datetime:
-        """Extracts date of image from EXIF"""
-
+        """Extracts date of image from EXIF."""
         try:
             img = Image.open(self.__path)
             self.__exif = img.getexif()
@@ -33,9 +33,8 @@ class ImageFile:
 
         return datetime.strptime(date_time_str, "%Y:%m:%d %H:%M:%S")
 
-    def move(self, output_path: Path):
-        """Moves file to new location"""
-
+    def move(self, output_path: Path) -> Path:
+        """Moves file to new location."""
         new_path = output_path
 
         i = 1
@@ -45,15 +44,14 @@ class ImageFile:
             new_path = output_path.parent.joinpath(f"{filename}-{i}{extention}")
             i += 1
 
-        print(f"{self.__path} -> {new_path}")
-
         # output_path.parent.mkdir(parents=True, exist_ok=True)
         # shutil.copy(self.__path, new_path)
         # shutil.move(self.__path, new_path)
 
-class ImageSorter:
-    """Performs images sorting operation"""
+        return new_path
 
+class ImageSorter:
+    """Performs images sorting operation."""
     def __init__(self, input_dir: Path):
         self.input_dir: Path = input_dir
         self.output_dir: Path = input_dir.joinpath("sort_output")
@@ -62,24 +60,36 @@ class ImageSorter:
         self.sort_unknown: bool = False
         self.rename_format: str = None
 
+        self.on_finish: Callable = None
+        self.on_skip: Callable[Path, Any] = None
+        self.on_move: Callable[[Path, Path], Any] = None
+        self.on_error: Callable[Exception, Any] = None
+
     def sort(self):
-        """Starts sorting loop"""
+        """Starts sorting loop."""
+        try:
+            for file in self.input_dir.iterdir():
+                if file.is_dir():
+                    if self.recursive:
+                        # If recursive, create new recursive sorter and perform sorting on directory
+                        img_sorter = copy.copy(self)
+                        img_sorter.input_dir = file
+                        img_sorter.on_error = self.on_error
+                        img_sorter.sort()
+                    else:
+                        continue
 
-        for file in self.input_dir.iterdir():
-            if file.is_dir():
-                if self.recursive:
-                    # If recursive, create new recursive sorter and perform sorting on directory
-                    img_sorter = copy.copy(self)
-                    img_sorter.input_dir = file
-                    img_sorter.sort()
-                else:
-                    continue
+                self.move(file)
+        except (PermissionError, FileNotFoundError) as e:
+            if self.on_error is not None:
+                self.on_error(e)
 
-            self.move(file)
+        if self.on_finish is not None:
+            self.on_finish()
+
 
     def move(self, path: Path):
-        """Determines new image path and moves the file"""
-
+        """Determines new image path and moves the file."""
         img = ImageFile(path)
 
         try:
@@ -95,8 +105,13 @@ class ImageSorter:
             if self.rename_format is not None:
                 filename = date_time.strftime(self.rename_format) + path.suffix
         elif date_time is None and not self.sort_unknown:
+            if self.on_skip is not None:
+                self.on_skip(path)
             return
 
         output_path = output_path.joinpath(filename)
 
-        img.move(output_path)
+        new_path = img.move(output_path)
+
+        if self.on_move is not None:
+            self.on_move(path, new_path)
